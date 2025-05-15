4f00ef9a82135e24b23890b5772e21b21c195786
anthiaggeli <anthi81990@hotmail.gr>
2025-05-15 13:14:08 +0300

    page beautification

--- BookRatingApp/BookRatingApp/Models/Review.cs
+++ BookRatingApp/BookRatingApp/Models/Review.cs
@@ -5,19 +5,19 @@ namespace BookRatingApp.Models
     public class Review
     {
         public int Id { get; set; }
-
+        [Display(Name = "Αξιολογητής")]
         [Required]
         public string Reviewer { get; set; }
-
+        [Display(Name = "Σχόλια")]
         [StringLength(500)]
         public string Comment { get; set; }
-
+        [Display(Name = "Βαθμολογία")]
         [Range(1, 5)]
         public int Rating { get; set; } // 1 to 5
         
         [Required]
         public int BookId { get; set; }
-
+        [Display(Name = "Βιβλίο")]
         public Book? Book { get; set; } // Σχέση με το Book (αλλά δεν απαιτείται να το στείλεις από τη φόρμα)
     
     }
